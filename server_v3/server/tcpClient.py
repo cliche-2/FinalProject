7755720a9d776ea3a9af5274c72493dc7806a8fb
cliche-2 <kaeunl33@gmail.com>
2018-05-08 00:22:27 +0900

    Update tcpClient.py
    covering info

--- server_v3/server/tcpClient.py
+++ server_v3/server/tcpClient.py
@@ -4,8 +4,7 @@ class TCPClient:
     def __init__(self):
         self.socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
            ## usage ## socket.connect(('ip',port)) 
-#        self.socket.connect(('192.168.219.104',35358))
-        self.socket.connect(('192.168.219.103',35358))
+        self.socket.connect(('ip',port))
 
     def sendAll(self,message):
         print("sent to admin >> ", message.decode())
